625ba605081c9969710eec79dd1c247d8248a573
Ian McGaunn <ianmcgaunn@Ians-MacBook-Air.local>
2013-07-23 09:42:08 -0400

    added documentation

--- python/fiducials.py
+++ python/fiducials.py
@@ -1,24 +1,30 @@
 # some functions for dealing with fiducials
-# a useful abstraction...
 
 import math
 
+# get_center grabs the four corners of the fiducial 
+# and calculates a center point that is the average of those
 def get_center(fiducial):
     xs = fiducial[1:8:2] # slice x coordinates
     ys = fiducial[2:9:2] # slice y coordinates
     center = [ (sum(xs) / len(xs)), (sum(ys) / len(ys)) ]
     return center
 
+# this grabs the orientation from a fiducial array
+# returned from roborealm.
 def get_orientation(fiducial):
     orientation = fiducial[14] * (math.pi / 180)
     return orientation
 
+# extract the fiducial's name given the index
+# into roborealm's FIDUCIALS_PATH and the lengh of the name
 def get_fiducial_name(fiducial, path):
     index = int(fiducial[15])
     length = int(fiducial[16])
     name = path[index:(length + index) - 1]
     return name
 
+# extracts scale from fiducial array
 def get_scale(fiducial):
     scale = float(fiducial[11])
     return scale
--- python/spider_track_corners.py
+++ python/spider_track_corners.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+# simple code to operate multiple spiders
 import array
 import time
 import math
@@ -7,28 +8,33 @@ import sys
 import os
 import serial
 
+# import Roborealm API Classes and fiducial functions
 from RR_API import RR_API
 import fiducials as fid
 
+# instantiate RR_API class and connet to the API
+# running on local computer
 rr = RR_API()
 rr.Connect("localhost")
 
 # this needs to be changed to the correct COM port
 # if it changes
 PORT = 'COM6'
-BAUD_RATE = 38400
+BAUD_RATE = 38400 # optimized for the panstamp
 
+# create serial object to communicate with transmitter
 ser = serial.Serial(PORT, BAUD_RATE)
 
 while True:
     try:
-        # get the fiducials from RR_API
+        # set reference coordinates
         x_r = 100
         y_r = 100
-        data = rr.GetAllVariables()
+        # get FIDUCIALS and FIDUCIALS_PATH variables from RR_API
         fiducials = rr.GetFiducials()
         fiducialsPath = rr.GetFiducialsPath()
 
+        # get orientation, position and name from every fiducial
         for key in fiducials.iterkeys():
             orientation = fid.get_orientation(fiducials[key])
             center = fid.get_center(fiducials[key])
@@ -36,6 +42,8 @@ while True:
             y = int(round(center[1]))
             name = fid.get_fiducial_name(fiducials[key], fiducialsPath)
 
+            # assign the correct panstamp address (5, 6, 7 or 8)
+            # to each fiducial and send different coordinates
             if name == 'balcony.gif':
                 spider = 5
                 x_r = 240
@@ -55,6 +63,8 @@ while True:
 
             print "spider number: {0}".format(spider)
             print "x: {0}, y: {1}, angle: {2}".format(x, y, orientation)
+            # write the coordinates to the transmitter and send them
+            # to the correct receiver
             ser.write("-./{0},{1},{2},{3},{4},{5}".format(spider, y_r, x_r,
                                                           y, x, (orientation * 1000)))
             time.sleep(.1)
@@ -66,4 +76,5 @@ while True:
         print "all done"
         break
 
+# close handle to transmitter object.
 ser.close()
